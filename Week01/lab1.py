edf7f0738bca968385891c0412df85c855ad0115
mii1na <minafahim2024@gmail.com>
2026-01-17 18:47:22 -0500

    lab 1

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -1,3 +1,28 @@
+#Q1
+# Sample Coding Questions 01 Week 01
+# Mina Fahim
+
+#Q2
+nums = [1, 4, 7, 9]
+
+#Q3
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Original expression:
+# e = a - b ** c // d + a % c
+
+# bracketed Version
+e = ((a - ((b ** c) // d)) + (a % c))
+
+#Q4
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+#Q5
 userAge = input("Enter your age")
 userAge = userAge + 22
 print ("now show the shop item filtered by age", userAge)
+
